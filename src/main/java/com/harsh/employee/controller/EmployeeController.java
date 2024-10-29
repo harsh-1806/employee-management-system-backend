68d1844c333b1e1c2bffaa6a7b5963250bd3f196
= <helloharsh51@gmail.com>
2024-10-29 20:58:31 +0530

    changed cors policy and environment variables

--- src/main/java/com/harsh/employee/controller/EmployeeController.java
+++ src/main/java/com/harsh/employee/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "https://employee-management-system-frontend-hazel.vercel.app/"})
 @RestController
 @RequestMapping("api/v1/")
 public class EmployeeController {
